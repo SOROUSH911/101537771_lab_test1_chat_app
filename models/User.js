085f8821db475906aab1b9b0d4cf1cb057fd7b73
Soroush Salari <soroush911@Soroushs-MacBook-Pro.local>
2026-02-11 18:37:22 -0500

    Add MongoDB schemas for User, GroupMessage, and PrivateMessage

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+    minlength: [3, 'Username must be at least 3 characters']
+  },
+  firstname: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: [true, 'Last name is required'],
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [4, 'Password must be at least 4 characters']
+  },
+  createon: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
